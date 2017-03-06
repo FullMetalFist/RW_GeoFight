611185a5114e51f1ab2895fa9ce29550d0c1cf72
Michael Vilabrera <m.vilabrera@gmail.com>
2017-03-06 16:55:35 -0500

    initial commit, chapter 1

---
+++ GeoFight/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  GeoFight
+//
+//  Created by Michael Vilabrera on 3/6/17.
+//  Copyright © 2017 Michael Vilabrera. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class GameViewController: UIViewController {
+    
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupScene()
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func setupView() {
+        scnView = self.view as! SCNView
+    }
+    
+    func setupScene() {
+        scnScene = SCNScene()
+        scnView.scene = scnScene
+    }
+}
